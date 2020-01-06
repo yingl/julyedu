b604743bd5e996a9ecdbdfde7c4b487f3d951cdf
yingl <linying_43151@163.com>
2020-01-06 19:25:43 +0800

    Tab/Space切换

--- java_ds/01_code/demo/com/demo/HelloWorld.java
+++ java_ds/01_code/demo/com/demo/HelloWorld.java
@@ -3,8 +3,8 @@ package com.demo;
 import com.demo.Human;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		Human human = new Human();
-		human.say();
-	}
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.say();
+    }
 }
--- java_ds/01_code/demo/com/demo/Human.java
+++ java_ds/01_code/demo/com/demo/Human.java
@@ -1,7 +1,7 @@
 package com.demo;
 
 public class Human {
-	public void say() {
-		System.out.println("Hello world");
+    public void say() {
+        System.out.println("Hello world");
 	}
 }
