bc7ea32ddcedbc1d0e5742b038de4efc8e265340
yingl <linying_43151@163.com>
2020-01-06 15:03:30 +0800

    java_ds 01 | Java基础：环境配置与基础语法 v0.1

---
+++ java_ds/01_code/demo/com/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+import com.demo.Human;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Human human = new Human();
+		human.say();
+	}
+}
---
+++ java_ds/01_code/demo/com/demo/Human.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+public class Human {
+	public void say() {
+		System.out.println("Hello world");
+	}
+}
