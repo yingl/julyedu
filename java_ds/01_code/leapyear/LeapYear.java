060c49a119a0580e77275e17db3e84c0e73b4763
yingl <linying_43151@163.com>
2020-01-07 14:33:12 +0800

    java_ds 01 | Java基础：环境配置与基础语法 v0.1 添加例子

---
+++ java_ds/01_code/leapyear/LeapYear.java
@@ -0,0 +1,18 @@
+import java.lang.Exception;
+import java.util.Scanner;
+
+class LeapYear {
+	public static void main(String[] args) {
+		int year;
+		Scanner scanner = new Scanner(System.in);
+		year = scanner.nextInt();
+		while ((year >= 0) && (year <= 3000)) {
+			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+				System.out.printf("%d is leap year\n", year);
+			} else {
+				System.out.printf("%d is not leap year\n", year);
+			}
+			year = scanner.nextInt();
+		}
+	}
+}
