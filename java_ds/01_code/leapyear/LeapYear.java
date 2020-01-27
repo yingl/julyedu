a00cb785affb5ec8fcdeffa802c5542008afc599
yingl <linying_43151@163.com>
2020-01-27 16:00:45 +0800

    Tab/Space切换

--- java_ds/01_code/leapyear/LeapYear.java
+++ java_ds/01_code/leapyear/LeapYear.java
@@ -2,17 +2,17 @@ import java.lang.Exception;
 import java.util.Scanner;
 
 class LeapYear {
-	public static void main(String[] args) {
-		int year;
-		Scanner scanner = new Scanner(System.in);
-		year = scanner.nextInt();
-		while ((year >= 0) && (year <= 3000)) {
-			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-				System.out.printf("%d is leap year\n", year);
-			} else {
-				System.out.printf("%d is not leap year\n", year);
-			}
-			year = scanner.nextInt();
-		}
-	}
+    public static void main(String[] args) {
+        int year;
+        Scanner scanner = new Scanner(System.in);
+        year = scanner.nextInt();
+        while ((year >= 0) && (year <= 3000)) {
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                System.out.printf("%d is leap year\n", year);
+            } else {
+                System.out.printf("%d is not leap year\n", year);
+            }
+            year = scanner.nextInt();
+        }
+    }
 }
